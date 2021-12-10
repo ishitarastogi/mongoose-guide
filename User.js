7a9da9a32a663e881c43d04026467314e6e803ae
ishitarastogi <ishita3099@gmail.com>
2021-12-10 20:11:12 +0530

    Adding validation

--- User.js
+++ User.js
@@ -1,7 +1,23 @@
-const mongoose=require('mongoose');
-
-const userSchema= new mongoose.Schema({
-    name:String,
-    age:Number
-})
-module.exports=mongoose.model("User", userSchema)
+const mongoose = require("mongoose");
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String,
+});
+const userSchema = new mongoose.Schema({
+  name: {type:String,minlength:5},
+  age: {type:Number,
+    min:1,
+    max:100,
+    validate:{
+    validator: v => v%2===0,
+    message:props => `${props.value} is not an even number`,
+    },
+},
+  email: { type: String, required: true, lowercase: true },
+  createdAt: { type: Date, immutable: true, default: () => Date.now() },
+  updatedAT: { type: Date, immutable: true, default: () => Date.now() },
+  bestFreind: mongoose.SchemaTypes.ObjectId,
+  hobbies: [String],
+  address: addressSchema,
+});
+module.exports = mongoose.model("User", userSchema);
--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const mongoose=require('mongoose');
-const User = require('./User');
-mongoose.connect('');
+const mongoose = require("mongoose");
+const User = require("./User");
+require("dotenv").config();
 
-run()
-async function run(){
-    const user=await User.create({name:"Ishita",age:26}) //create new user and save data
-// const user=new User({name:"ishita",age:23}) create new user ,stored locally
-// await user.save() //it will save data to mongoose and return a promise
-console.log(user)
+mongoose.connect(process.env.URL);
+
+run();
+async function run() {
+  try {
+    const user = await User.create({
+      name: "Ishita",
+      age: 22,
+      email: "abc@abc.com",
+      hobbies: ["writing", "music"],
+      address: {
+        street: "tilak marg",
+      },
+    });
+    console.log(user);
+  } catch (e) {
+    console.log(e.message);
+  }
+  //create new user and save data
+  // const user=new    User({name:"ishita",age:23}) create new user ,stored locally
+  // await user.save() //it will save data to mongoose and return a promise
+  // user.name="ishu" //update user name
+  // await user.save()
 }
